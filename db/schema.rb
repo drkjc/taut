5fa959c5020d12b37588abaae4cb8936627b51de
github username <github email address>
2019-06-14 11:26:10 -0700

    migrated messages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_14_181222) do
+ActiveRecord::Schema.define(version: 2019_06_14_182246) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "username"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2019_06_14_181222) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "contact_id"
+    t.integer "group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
