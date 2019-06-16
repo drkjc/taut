6462d2ef237ccd7b5c4c5addbe9856a13aa4e7fd
github username <github email address>
2019-06-16 16:49:34 -0700

    rendered group layout to messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :find_user, only: [:index, :show, :edit]
+
+  def index
+    render template: "groups/index"
+  end
+
+  private
+
+  def find_user
+    @user = User.find(session[:id])
+  end
 end
