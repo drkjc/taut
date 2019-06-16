9425eb5e41351191faeec3337347d85b25eb262a
github username <github email address>
2019-06-16 15:37:43 -0700

    contact show path added

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,23 @@
 class ContactsController < ApplicationController
+  before_action :find_user, only: [:show]
+
+  def show
+    @contact = Contact.find(params[:id])
+
+    messages = []
+
+    messages << sent = Message.where(user_id: @user.id, contact_id: params[:id])
+
+    messages << received = Message.where(user_id: params[:id], contact_id: @user.id)
+
+    @conversation = messages.flatten.sort_by { |m| m.created_at}.uniq
+
+  end
+
+  private
+
+  def find_user
+    @user = User.find(session[:id])
+  end
+
 end
