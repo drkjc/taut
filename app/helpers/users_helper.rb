5928fa4a29572c1ddc6ae61791960824763e1e09
github username <github email address>
2019-07-04 11:01:42 -0700

    received messages show in contacts menu

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -13,9 +13,19 @@ module UsersHelper
   end
 
   def get_contacts
+    get_sent_message_contact | get_received_message_user
+  end
+
+  def get_received_message_user
     Message.where(contact_id: @user.id).map do |message|
       message.user
-    end.uniq
+    end
+  end
+
+  def get_sent_message_contact
+    Message.where(user_id: @user.id).map do |message|
+      message.contact
+    end
   end
 
 end
